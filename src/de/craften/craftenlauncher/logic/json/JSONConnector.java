0dbb2102f43ab1082da69d42f4d7f39c422694a2
Sascha Becker <s.becker@wertarbyte.com>
2014-05-10 23:20:37 +0200

    Added license

--- src/de/craften/craftenlauncher/logic/json/JSONConnector.java
+++ src/de/craften/craftenlauncher/logic/json/JSONConnector.java
@@ -1,3 +1,23 @@
+/**
+ * CraftenLauncher is an alternative Launcher for Minecraft developed by Mojang.
+ * Copyright (C) 2013  Johannes "redbeard" Busch, Sascha "saschb2b" Becker
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author saschb2b
+ */
+
 package de.craften.craftenlauncher.logic.json;
 
 import de.craften.craftenlauncher.logic.Logger;
@@ -9,9 +29,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-/**
- * Created by Saschb2b on 10.05.2014.
- */
 public class JSONConnector {
     public static String executePost(String targetURL, String urlParameters) {
         URL url;
